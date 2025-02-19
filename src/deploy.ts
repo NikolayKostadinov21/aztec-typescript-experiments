4594caa7bcbbbd3ff62a6ab6cf6e9294d1cfb21e
Nikolay Kostadinov <nikolaykostadinov21@gmail.com>
2025-02-19 14:26:55 +0200

    feat(src): Implement deployment script

---
+++ src/deploy.ts
@@ -0,0 +1,40 @@
+import { MainContract } from "./target/Main.js"
+import { AccountWallet, CompleteAddress, createLogger, Fr, PXE, waitForPXE, createPXEClient, Logger } from "@aztec/aztec.js";
+import { getSchnorrAccount } from '@aztec/accounts/schnorr';
+import { deriveSigningKey } from '@aztec/circuits.js';
+import { getInitialTestAccountsWallets } from "@aztec/accounts/testing";
+
+const setupSandbox = async () => {
+    const { PXE_URL = 'http://localhost:8080' } = process.env;
+    const pxe = await createPXEClient(PXE_URL);
+    await waitForPXE(pxe);
+    return pxe;
+};
+
+async function main() {
+    console.log('here')
+    let pxe: PXE;
+    let wallets: AccountWallet[] = [];
+    let accounts: CompleteAddress[] = [];
+    let logger: Logger;
+
+    logger = createLogger('aztec:aztec-typescript-experiments');
+    console.log('after logger')
+
+    pxe = await setupSandbox();
+    console.log('pxe', pxe)
+    wallets = await getInitialTestAccountsWallets(pxe);
+
+    let secretKey = Fr.random();
+    let salt = Fr.random();
+
+    let schnorrAccount = await getSchnorrAccount(pxe, secretKey, deriveSigningKey(secretKey), salt);
+    const { address, publicKeys, partialAddress } = await schnorrAccount.getCompleteAddress();
+    let tx = await schnorrAccount.deploy().wait();
+    let wallet = await schnorrAccount.getWallet();
+
+    const mainContract = await MainContract.deploy(wallet).send().deployed();
+    logger.info(`Main Contract deployed at: ${mainContract.address}`);
+}
+
+main();
